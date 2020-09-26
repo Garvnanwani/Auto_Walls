956f9765e9b96ccc8844cbddb9b6b91e41b9bc27
Garv nanwani <garvnanwani88@gmail.com>
2020-09-26 16:59:18 +0530

    Update autowalls.py

--- autowalls.py
+++ autowalls.py
@@ -5,11 +5,11 @@ import time
 
 
 def getWall():
-    access_key = "Y5MVF2GHEwKss-KIWb241FZ8EiG43j7pyLi3wTYvtYU"
+    access_key = "" #Put your unsplash API access_key here
     url = "https://api.unsplash.com/photos/random/?client_id=" + access_key
     params = {
-        "query": "HD Wallpapers",
-        "orientation": "landscape",
+        "query": "HD Wallpapers",  #you can set any query you want
+        "orientation": "landscape", #landscape photos will be best suited for wallpapers
     }
     response = requests.get(url, params).json()
     image_url = response['urls']['full']
@@ -19,16 +19,16 @@ def getWall():
 
 def setWal():
     wallpaper = getWall()
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, wallpaper, 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, wallpaper, 0)  #this function helps to change the wallpaper through windows commands, you can do similar things for mac or linux.
 
 
 def run_script():
     try:
         while True:
             setWal()
-            time.sleep(5)
+            time.sleep(5)   #here you can set the time interval after which you want the wallpaper to change
     except KeyboardInterrupt:
-        print("\nHope you liked the wallpaper")
+        print("\nHope you liked the wallpaper") #Whenever you like any wallpaper, you can close the script to keep the wallpaper
     except Exception:
         pass
 
